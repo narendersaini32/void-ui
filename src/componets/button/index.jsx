eddb74682672b555d9692bd1a58a9915e8ba4887
narender saini <narendersaini32@gmail.com>
2019-05-02 15:50:42 +0530

    new colors added

--- src/componets/button/index.jsx
+++ src/componets/button/index.jsx
@@ -4,28 +4,29 @@ import PropTypes from 'prop-types';
 import colors from '../../consts/colors/colors.json';
 
 const StyledButton = styled.button`
-    border:${props => `${props.border}px solid ${props.textColor}`};
-    margin:10px;
-    cursor: pointer;
-    background: ${props => props.primaryColor};
-    border-radius: 7px;
-    box-shadow: ${props => `${props.boxShadow}px ${props.boxShadow}px`};
-    color: ${props => props.textColor};
-    font-size: 19px;
-    outline: none;
-    text-align: center;
-    user-select: none;
-    transition: all 0.2s ease;
-    padding: 7px 15px 7px 15px;
-    text-transform: capitalize;
-    &:active{
-    top:4px;
-    position:relative;
-    box-shadow: 0px 0px ;
-    }
-    &:hover {
-    opacity:0.8;
-    }
+font-family: 'Dancing Script', cursive;
+  border: ${props => `${props.border}px solid ${props.textColor}`};
+  margin: 10px;
+  cursor: pointer;
+  background: ${props => props.primaryColor};
+  border-radius: 7px;
+  box-shadow: ${props => `${props.boxShadow}px ${props.boxShadow}px`};
+  color: ${props => props.textColor};
+  font-size: 19px;
+  outline: none;
+  text-align: center;
+  user-select: none;
+  transition: all 0.2s ease;
+  padding: 7px 15px 7px 15px;
+  text-transform: capitalize;
+  &:active {
+    top: 4px;
+    position: relative;
+    box-shadow: 0px 0px;
+  }
+  &:hover {
+    opacity: 0.8;
+  }
 `;
 const Button = ({
   text, color, textColor, border, boxShadow,
@@ -33,12 +34,13 @@ const Button = ({
   const colorNumber = ['400', '500'];
   const primaryColor = colors[color + colorNumber[1]] || color;
   const secondaryColor = colors[color + colorNumber[0]] || color;
+  const textColorFinal = colors[textColor + colorNumber[0]] || textColor;
   return (
     <StyledButton
       type="button"
       secondaryColor={secondaryColor}
       primaryColor={primaryColor}
-      textColor={textColor}
+      textColor={textColorFinal}
       border={border}
       boxShadow={boxShadow}
     >
--- src/componets/home/index.jsx
+++ src/componets/home/index.jsx
@@ -15,21 +15,31 @@ const Main = styled.div`
 
 const Home = () => (
   <Main>
-    <Container title="white background">
-      <Button text="Default" textColor="grey" color="white" />
-      <Button text="yellow" textColor="yellow" color="white" />
-      <Button text="red" textColor="red" color="white" />
-      <Button text="pink" textColor="pink" color="white" />
-      <Button text="cyan" textColor="cyan" color="white" />
-      <Button text="indigo!" textColor="indigo" color="white" />
-    </Container>
     <Container title="Black Text Color">
       <Button text="Default" />
       <Button text="yellow" color="yellow" />
       <Button text="red" color="red" />
       <Button text="pink" color="pink" />
       <Button text="cyan" color="cyan" />
-      <Button text="indigo!" color="indigo" />
+      <Button text="indigo" color="indigo" />
+      <Button text="blue" color="blue" />
+      <Button text="yellowvivid" color="yellowvivid" />
+      <Button text="bluregrey" color="bluregrey" />
+
+
+    </Container>
+    <Container title="white background">
+      <Button text="Default" textColor="grey" color="white" />
+      <Button text="yellow" textColor="yellow" color="white" />
+      <Button text="red" textColor="red" color="white" />
+      <Button text="pink" textColor="pink" color="white" />
+      <Button text="cyan" textColor="cyan" color="white" />
+      <Button text="indigo" textColor="indigo" color="white" />
+      <Button text="blue" textColor="blue" color="white" />
+      <Button text="yellowvivid" textColor="yellowvivid" color="white" />
+      <Button text="bluregrey" textColor="bluregrey" color="white" />
+
+
     </Container>
     <Container title="white text color">
       <Button text="Default" textColor="white" />
@@ -37,7 +47,12 @@ const Home = () => (
       <Button text="red" color="red" textColor="white" />
       <Button text="pink" color="pink" textColor="white" />
       <Button text="cyan" color="cyan" textColor="white" />
-      <Button text="indigo!" color="indigo" textColor="white" />
+      <Button text="indigo" color="indigo" textColor="white" />
+      <Button text="blue" color="blue" textColor="white" />
+      <Button text="yellowvivid" color="yellowvivid" textColor="white" />
+      <Button text="bluregrey" color="bluregrey" textColor="white" />
+
+
     </Container>
     <Container title="with border and box shadow">
       <Button text="Default" textColor="white" border={1} />
@@ -45,7 +60,11 @@ const Home = () => (
       <Button text="red" textColor="red" color="white" border={1} />
       <Button text="pink" textColor="pink" color="white" border={1} />
       <Button text="cyan" textColor="cyan" color="white" border={1} />
-      <Button text="indigo!" textColor="indigo" color="white" border={1} />
+      <Button text="indigo" textColor="indigo" color="white" border={1} />
+      <Button text="blue" textColor="blue" color="white" border={1} />
+      <Button text="yellowvivid" textColor="yellowvivid" color="white" border={1} />
+      <Button text="bluregrey" textColor="bluregrey" color="white" border={1} />
+
     </Container>
     <Container title="without boxShadow">
       <Button text="Default" textColor="white" boxShadow={0} />
@@ -53,7 +72,11 @@ const Home = () => (
       <Button text="red" textColor="red" color="white" boxShadow={0} />
       <Button text="pink" textColor="pink" color="white" boxShadow={0} />
       <Button text="cyan" textColor="cyan" color="white" boxShadow={0} />
-      <Button text="indigo!" textColor="indigo" color="white" boxShadow={0} />
+      <Button text="indigo" textColor="indigo" color="white" boxShadow={0} />
+      <Button text="blue" textColor="blue" color="white" boxShadow={0} />
+      <Button text="yellowvivid" textColor="yellowvivid" color="white" boxShadow={0} />
+      <Button text="bluregrey" textColor="bluregrey" color="white" boxShadow={0} />
+
     </Container>
     <Container title="without boxShadow and with border">
       <Button text="Default" textColor="white" boxShadow={0} border={1} />
@@ -61,8 +84,13 @@ const Home = () => (
       <Button text="red" textColor="red" color="white" boxShadow={0} border={1} />
       <Button text="pink" textColor="pink" color="white" boxShadow={0} border={1} />
       <Button text="cyan" textColor="cyan" color="white" boxShadow={0} border={1} />
-      <Button text="indigo!" textColor="indigo" color="white" boxShadow={0} border={1} />
+      <Button text="indigo" textColor="blue" color="white" boxShadow={0} border={1} />
+      <Button text="blue" textColor="blue" color="white" boxShadow={0} border={1} />
+      <Button text="yellowvivid" textColor="yellowvivid" color="white" boxShadow={0} border={1} />
+      <Button text="bluregrey" textColor="bluregrey" color="white" boxShadow={0} border={1} />
+
     </Container>
+
   </Main>
 );
 
