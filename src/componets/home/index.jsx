df2d66fc35a3c7562817b6ebe6e809eec52bdb0a
narender saini <narendersaini32@gmail.com>
2019-05-02 16:48:00 +0530

    updated colors

--- src/componets/home/index.jsx
+++ src/componets/home/index.jsx
@@ -24,25 +24,25 @@ const Home = () => (
       <Button text="indigo" color="indigo" />
       <Button text="blue" color="blue" />
       <Button text="yellowvivid" color="yellowvivid" />
-      <Button text="bluregrey" color="bluregrey" />
+      <Button text="bluegrey" color="bluegrey" />
 
 
     </Container>
     <Container title="white background">
-      <Button text="Default" textColor="grey" color="white" />
-      <Button text="yellow" textColor="yellow" color="white" />
-      <Button text="red" textColor="red" color="white" />
-      <Button text="pink" textColor="pink" color="white" />
-      <Button text="cyan" textColor="cyan" color="white" />
-      <Button text="indigo" textColor="indigo" color="white" />
-      <Button text="blue" textColor="blue" color="white" />
-      <Button text="yellowvivid" textColor="yellowvivid" color="white" />
-      <Button text="bluregrey" textColor="bluregrey" color="white" />
+      <Button text="Default" textColor="grey" />
+      <Button text="yellow" textColor="yellow" />
+      <Button text="red" textColor="red" />
+      <Button text="pink" textColor="pink" />
+      <Button text="cyan" textColor="cyan" />
+      <Button text="indigo" textColor="indigo" />
+      <Button text="blue" textColor="blue" />
+      <Button text="yellowvivid" textColor="yellowvivid" />
+      <Button text="bluegrey" textColor="bluegrey" />
 
 
     </Container>
     <Container title="white text color">
-      <Button text="Default" textColor="white" />
+      <Button text="Default" textColor="black" />
       <Button text="yellow" color="yellow" textColor="white" />
       <Button text="red" color="red" textColor="white" />
       <Button text="pink" color="pink" textColor="white" />
@@ -50,59 +50,47 @@ const Home = () => (
       <Button text="indigo" color="indigo" textColor="white" />
       <Button text="blue" color="blue" textColor="white" />
       <Button text="yellowvivid" color="yellowvivid" textColor="white" />
-      <Button text="bluregrey" color="bluregrey" textColor="white" />
+      <Button text="bluegrey" color="bluegrey" textColor="white" />
 
 
     </Container>
     <Container title="with border and box shadow">
-      <Button text="Default" textColor="white" border={1} />
-      <Button text="yellow" textColor="yellow" color="white" border={1} />
-      <Button text="red" textColor="red" color="white" border={1} />
-      <Button text="pink" textColor="pink" color="white" border={1} />
-      <Button text="cyan" textColor="cyan" color="white" border={1} />
-      <Button text="indigo" textColor="indigo" color="white" border={1} />
-      <Button text="blue" textColor="blue" color="white" border={1} />
-      <Button text="yellowvivid" textColor="yellowvivid" color="white" border={1} />
-      <Button text="bluregrey" textColor="bluregrey" color="white" border={1} />
+      <Button text="Default" textColor="black" border={1} />
+      <Button text="yellow" textColor="yellow" border={1} />
+      <Button text="red" textColor="red" border={1} />
+      <Button text="pink" textColor="pink" border={1} />
+      <Button text="cyan" textColor="cyan" border={1} />
+      <Button text="indigo" textColor="indigo" border={1} />
+      <Button text="blue" textColor="blue" border={1} />
+      <Button text="yellowvivid" textColor="yellowvivid" border={1} />
+      <Button text="bluegrey" textColor="bluegrey" border={1} />
 
     </Container>
     <Container title="without boxShadow">
-      <Button text="Default" textColor="white" boxShadow={0} />
-      <Button text="yellow" textColor="yellow" color="white" boxShadow={0} />
-      <Button text="red" textColor="red" color="white" boxShadow={0} />
-      <Button text="pink" textColor="pink" color="white" boxShadow={0} />
-      <Button text="cyan" textColor="cyan" color="white" boxShadow={0} />
-      <Button text="indigo" textColor="indigo" color="white" boxShadow={0} />
-      <Button text="blue" textColor="blue" color="white" boxShadow={0} />
-      <Button text="yellowvivid" textColor="yellowvivid" color="white" boxShadow={0} />
-      <Button text="bluregrey" textColor="bluregrey" color="white" boxShadow={0} />
+      <Button text="Default" textColor="black" boxShadow={0} />
+      <Button text="yellow" textColor="yellow" boxShadow={0} />
+      <Button text="red" textColor="red" boxShadow={0} />
+      <Button text="pink" textColor="pink" boxShadow={0} />
+      <Button text="cyan" textColor="cyan" boxShadow={0} />
+      <Button text="indigo" textColor="indigo" boxShadow={0} />
+      <Button text="blue" textColor="blue" boxShadow={0} />
+      <Button text="yellowvivid" textColor="yellowvivid" boxShadow={0} />
+      <Button text="bluegrey" textColor="bluegrey" boxShadow={0} />
 
     </Container>
-    <Container title="without boxShadow and with border">
-      <Button text="Default" textColor="white" boxShadow={0} border={1} />
-      <Button text="yellow" textColor="yellow" color="white" boxShadow={0} border={1} />
-      <Button text="red" textColor="red" color="white" boxShadow={0} border={1} />
-      <Button text="pink" textColor="pink" color="white" boxShadow={0} border={1} />
-      <Button text="cyan" textColor="cyan" color="white" boxShadow={0} border={1} />
-      <Button text="indigo" textColor="blue" color="white" boxShadow={0} border={1} />
-      <Button text="blue" textColor="blue" color="white" boxShadow={0} border={1} />
-      <Button text="yellowvivid" textColor="yellowvivid" color="white" boxShadow={0} border={1} />
-      <Button text="bluregrey" textColor="bluregrey" color="white" boxShadow={0} border={1} />
 
-    </Container>
     <Container title="rounded buttons">
-      <Button text="Default" textColor="white" borderRadius={20} />
-      <Button text="yellow" textColor="yellow" color="white" borderRadius={20} />
-      <Button text="red" textColor="red" color="white" borderRadius={20} />
-      <Button text="pink" textColor="pink" color="white" borderRadius={20} />
-      <Button text="cyan" textColor="cyan" color="white" borderRadius={20} />
-      <Button text="indigo" textColor="blue" color="white" borderRadius={20} />
-      <Button text="blue" textColor="blue" color="white" borderRadius={20} />
-      <Button text="yellowvivid" textColor="yellowvivid" color="white" borderRadius={20} />
-      <Button text="bluregrey" textColor="bluregrey" color="white" borderRadius={20} />
+      <Button text="Default" textColor="black" borderRadius={20} />
+      <Button text="yellow" textColor="yellow" borderRadius={20} />
+      <Button text="red" textColor="red" borderRadius={20} />
+      <Button text="pink" textColor="pink" borderRadius={20} />
+      <Button text="cyan" textColor="cyan" borderRadius={20} />
+      <Button text="indigo" textColor="blue" borderRadius={20} />
+      <Button text="blue" textColor="blue" borderRadius={20} />
+      <Button text="yellowvivid" textColor="yellowvivid" borderRadius={20} />
+      <Button text="bluegrey" textColor="bluegrey" borderRadius={20} />
 
     </Container>
-
   </Main>
 );
 
